267e8161420ffc0feaa2c2066e0180d7e3f96e29
Zhifeng Hu <zhifeng.hu@outlook.com>
2023-04-26 21:51:25 +0800

    Add headers for request

--- main.py
+++ main.py
@@ -95,6 +95,12 @@ def main():
     ts = datetime.datetime.now().timestamp()
     conf = load_feed_conf()
     items = []
+    request_timeout = 30
+    ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    headers = {
+        'user-agent': ua
+    }
+    feedparser.USER_AGENT = ua
 
     logger = logging.getLogger(__name__)
     logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
@@ -107,7 +113,7 @@ def main():
                 continue
             try:
                 logger.debug(feed['url'])
-                d = feedparser.parse(feed['url'], modified=yesterday)
+                d = feedparser.parse(feed['url'], modified=yesterday, request_headers=headers)
             except Exception as e:
                 logger.warning(e)
                 continue
@@ -182,14 +188,14 @@ def main():
                         payload['variables']['month'] = today.month
                         payload['variables']['day'] = today.day
                     try:
-                        res = requests.post(url, json=payload, timeout=30)
+                        res = requests.post(url, json=payload, timeout=request_timeout, headers=headers)
                         res.raise_for_status()
                     except requests.exceptions.RequestException as e:
                         logger.warning(e)
                         continue
                 else:
                     try:
-                        res = requests.post(url, timeout=30)
+                        res = requests.post(url, timeout=request_timeout, headers=headers)
                         res.raise_for_status()
                     except requests.exceptions.RequestException as e:
                         logger.warning(e)
@@ -198,7 +204,7 @@ def main():
                 if 'GitHub Advanced Search' == api['channel']:
                     url = url.format(date=lastweek.strftime('%Y-%m-%d'))
                 try:
-                    res = requests.get(url, timeout=30)
+                    res = requests.get(url, timeout=request_timeout, headers=headers)
                     res.raise_for_status()
                 except requests.exceptions.RequestException as e:
                     logger.warning(e)
@@ -292,7 +298,7 @@ def main():
             url = quote['url']
             try:
                 logger.debug(url)
-                res = requests.get(url, timeout=30)
+                res = requests.get(url, timeout=request_timeout, headers=headers)
                 res.raise_for_status()
             except requests.exceptions.RequestException as e:
                 logger.warning(e)
