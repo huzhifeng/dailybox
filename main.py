1df6580742ad3761012d691a44cd9aa5bdfec5f7
huzhifeng <hu_zhifeng@qq.com>
2023-04-30 21:39:47 +0800

    pylint

--- main.py
+++ main.py
@@ -4,111 +4,42 @@ import shutil
 from pathlib import Path
 import json
 import logging
-import pytz
+import urllib
+import socket
 import time
 import datetime
 import dateutil.parser
 import dateparser
+import pytz
 import feedparser
 import requests
-import urllib
-
-
-def load_feed_conf():
-    """Load configuration."""
-    with open('feed.json', mode='r', encoding='utf-8') as f:
-        conf = json.load(f)
-
-    return conf
-
-
-def publish_md(items):
-    """Publish to markdown."""
-    categories_obj = {}
-    tags_obj = {}
-    today_str = datetime.datetime.today().strftime('%Y%m%d')
-    fname_daily_box = f'archives/daily-box-{today_str}.md'
-    md_daily_box = f'# Daily Box {today_str}\n\n'
-
-    for item in items:
-        category = item['category']
-        if not category in categories_obj:
-            categories_obj[category] = ''
-
-        tags = item['tags'] if 'tags' in item else [];
-        for tag in tags:
-            if not tag in tags_obj:
-                tags_obj[tag] = ''
-
-        md_entry = ''
-        if '语录' == category:
-            if item['origin']:
-                md_entry = f'- "{item["content"]}" - {item["author"]} 《{item["origin"]}》\n'
-            else:
-                md_entry = f'- "{item["content"]}" - {item["author"]}\n'
-        else:
-            md_entry = f'- [{item["channel"]}]({item["portal"]}) | [{item["title"]}]({item["link"]})\n'
-
-        categories_obj[category] += md_entry
-        for tag in tags:
-            tags_obj[tag] += md_entry
-
-    for category in categories_obj:
-        md_daily_box += f'## {category}\n'
-        md_daily_box += f'{categories_obj[category]}\n'
-        md_category = f'## {today_str}\n{categories_obj[category]}\n'
-        Path('categories').mkdir(parents=True, exist_ok=True)
-        fname_category = f'categories/{category}.md'
-        md_old = ''
-        if os.path.isfile(fname_category):
-            with open(fname_category, mode='r', encoding='utf-8') as fd_category_old:
-                md_old = fd_category_old.read()
-        with open(fname_category, mode='w', encoding='utf-8') as fd_category_new:
-            fd_category_new.write(f'{md_category}{md_old}')
-
-    md_daily_box += 'EOF'
-    print(md_daily_box)
-
-    Path('archives').mkdir(parents=True, exist_ok=True)
-    if not os.path.isfile(fname_daily_box):
-        fd_daily_box = open(fname_daily_box, mode='w', encoding='utf-8')
-        fd_daily_box.write(md_daily_box)
-        fd_daily_box.close()
-        shutil.copy(fname_daily_box, 'README.md')
-
-    for tag in tags_obj:
-        md_tag = f'## {today_str}\n{tags_obj[tag]}\n'
-        Path('tags').mkdir(parents=True, exist_ok=True)
-        fname_tag = f'tags/{tag}.md'
-        md_old = ''
-        if os.path.isfile(fname_tag):
-            with open(fname_tag, mode='r', encoding='utf-8') as fd_tag_old:
-                md_old = fd_tag_old.read()
-        with open(fname_tag, mode='w', encoding='utf-8') as fd_tag_new:
-            fd_tag_new.write(f'{md_tag}{md_old}')
 
 
 def main():
     """Main loop."""
-    tz = pytz.timezone('Asia/Shanghai')
-    now = datetime.datetime.now(tz)
+    timezone = pytz.timezone('Asia/Shanghai')
+    now = datetime.datetime.now(timezone)
     today = now.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
     yesterday = today - datetime.timedelta(days=1)
     lastweek = today - datetime.timedelta(weeks=1)
-    ts = datetime.datetime.now().timestamp()
-    conf = load_feed_conf()
+    timestamp = datetime.datetime.now().timestamp()
     items = []
     request_timeout = 30
-    ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     request_headers = {
-        'user-agent': ua
+        'user-agent': user_agent
     }
-    feedparser.USER_AGENT = ua
+    feedparser.USER_AGENT = user_agent
+    socket.setdefaulttimeout(request_timeout)
 
     logger = logging.getLogger(__name__)
     logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
     console_handler = logging.StreamHandler()
     logger.addHandler(console_handler)
+
+    conf = {}
+    with open('feed.json', mode='r', encoding='utf-8') as f:
+        conf = json.load(f)
 
     if 'feed' in conf:
         for feed in conf['feed']:
@@ -116,9 +47,10 @@ def main():
                 continue
             try:
                 logger.debug(feed['url'])
-                d = feedparser.parse(feed['url'], modified=yesterday, request_headers=request_headers)
-            except Exception as e:
-                logger.warning(e)
+                d = feedparser.parse(
+                    feed['url'], modified=yesterday, request_headers=request_headers)
+            except Exception as e_parse:
+                logger.warning(e_parse)
                 continue
             updated = d.feed.get('updated_parsed', today)
             if isinstance(updated, time.struct_time):
@@ -145,7 +77,8 @@ def main():
                 elif '泰晓资讯' == feed['channel']:
                     if not '泰晓资讯' in entry.title:
                         continue
-                elif 'Hacker News' == feed['channel'] or 'Lobsters' == feed['channel']:
+                elif 'Hacker News' == feed['channel'] \
+                    or 'Lobsters' == feed['channel']:
                     if 'comments' in entry:
                         entry.link = entry.comments
                 elif 'Slashdot' == feed['channel']:
@@ -162,10 +95,11 @@ def main():
                     if '喷嚏网' == feed['channel']:
                         if 'Wes' in entry.published:
                             # typo, Wes should be Wed
-                            published = dateparser.parse(entry.published.replace('Wes', 'Wed'))
+                            published = dateparser.parse(
+                                entry.published.replace('Wes', 'Wed'))
                             published = published.replace(tzinfo=None)
                     else:
-                            continue
+                        continue
                 if published < yesterday:
                     continue
                 item = {
@@ -176,7 +110,8 @@ def main():
                     'title': entry.title,
                     'link': entry.link
                 }
-                if '周刊' in item['title'] or 'Weekly'.casefold() in item['title'].casefold():
+                if '周刊' in item['title'] \
+                    or 'Weekly'.casefold() in item['title'].casefold():
                     item['tags'].append('周刊')
                 items.append(item)
                 i = i + 1
@@ -193,45 +128,48 @@ def main():
                 if 'payload' in api['request']:
                     payload = api['request']['payload']
                     if '8点1氪' == api['channel']:
-                        payload['timestamp'] = ts * 1000
+                        payload['timestamp'] = timestamp * 1000
                     elif 'Product Hunt' == api['channel']:
                         payload['variables']['year'] = today.year
                         payload['variables']['month'] = today.month
                         payload['variables']['day'] = yesterday.day
                     try:
-                        res = requests.post(url, json=payload, timeout=request_timeout, headers=request_headers)
-                        res.raise_for_status()
-                    except requests.exceptions.RequestException as e:
-                        logger.warning(e)
+                        resp = requests.post(
+                            url, json=payload, timeout=request_timeout, headers=request_headers)
+                        resp.raise_for_status()
+                    except requests.exceptions.RequestException as e_req:
+                        logger.warning(e_req)
                         continue
                 else:
                     try:
-                        res = requests.post(url, timeout=request_timeout, headers=request_headers)
-                        res.raise_for_status()
-                    except requests.exceptions.RequestException as e:
-                        logger.warning(e)
+                        resp = requests.post(
+                            url, timeout=request_timeout, headers=request_headers)
+                        resp.raise_for_status()
+                    except requests.exceptions.RequestException as e_req:
+                        logger.warning(e_req)
                         continue
             else:
                 if 'GitHub Advanced Search' == api['channel']:
                     url = url.format(date=lastweek.strftime('%Y-%m-%d'))
                 try:
-                    res = requests.get(url, timeout=request_timeout, headers=request_headers)
-                    res.raise_for_status()
-                except requests.exceptions.RequestException as e:
-                    logger.warning(e)
+                    resp = requests.get(
+                        url, timeout=request_timeout, headers=request_headers)
+                    resp.raise_for_status()
+                except requests.exceptions.RequestException as e_req:
+                    logger.warning(e_req)
                     continue
-            resp = res.json()
+            resp_json = resp.json()
             keys = api['response']['list'].split('.')
             l = len(keys)
-            if l == 1 and keys[0] in resp:
-                entries = resp[keys[0]]
-            elif l == 2 and keys[0] in resp and keys[1] in resp[keys[0]]:
-                entries = resp[keys[0]][keys[1]]
-            elif l == 3 and keys[0] in resp and keys[1] in resp[keys[0]] \
-                and keys[2] in resp[keys[0]][keys[1]]:
-                entries = resp[keys[0]][keys[1]][keys[2]]
+            if l == 1 and keys[0] in resp_json:
+                entries = resp_json[keys[0]]
+            elif l == 2 and keys[0] in resp_json and keys[1] in resp_json[keys[0]]:
+                entries = resp_json[keys[0]][keys[1]]
+            elif l == 3 and keys[0] in resp_json and keys[1] in resp_json[keys[0]] \
+                    and keys[2] in resp_json[keys[0]][keys[1]]:
+                entries = resp_json[keys[0]][keys[1]][keys[2]]
             else:
-                logger.debug(f'{url} response error')
+                logger.debug('%s response error', url)
                 continue
 
             title = api['entry']['title']
@@ -258,11 +196,16 @@ def main():
                             entry[date])
                 else:
                     if '晚点早知道' == api['channel']:
-                        published = dateparser.parse(entry[date])  # '今天' or '昨天'
-                    elif '竹白先锋作品' == api['channel'] or '竹白新锐作品' == api['channel'] or '竹白上周热门' == api['channel']:
+                        published = dateparser.parse(
+                            entry[date])  # '今天' or '昨天'
+                    elif '竹白先锋作品' == api['channel'] \
+                        or '竹白新锐作品' == api['channel'] \
+                        or '竹白上周热门' == api['channel']:
                         # '1.9小时前' or '1680614161357'
                         published = dateparser.parse(entry[date])
-                    elif 'GitHub中文社区' == api['channel'] or 'GitHub Advanced Search' == api['channel'] or 'Product Hunt' == api['channel']:
+                    elif 'GitHub中文社区' == api['channel'] \
+                        or 'GitHub Advanced Search' == api['channel'] \
+                        or 'Product Hunt' == api['channel']:
                         # '2023-04-07T10:38:28Z' or '2023-04-10T00:01:00-07:00'
                         published = dateparser.parse(entry[date])
                         published = published.replace(tzinfo=None)
@@ -285,7 +228,8 @@ def main():
                 link_map = {placeholder: entry[placeholder]}
                 if '微博热搜' == api['channel']:
                     # 中文和空格需要做 URL 编码，例如 '长月烬明 抄袭阴阳师'
-                    link_map[placeholder] = urllib.parse.quote(entry[placeholder])
+                    link_map[placeholder] = urllib.parse.quote(
+                        entry[placeholder])
                 item = {
                     'category': api['category'],
                     'tags': api['tags'],
@@ -296,7 +240,8 @@ def main():
                 }
                 if 'Product Hunt' == api['channel']:
                     if '_title_prefix' in entry and '_links' in entry:
-                        item['title'] = entry['_title_prefix'].removesuffix(' - ')
+                        item['title'] = entry['_title_prefix'].removesuffix(
+                            ' - ')
                         item['link'] = entry['_links'][0]['url']
                 items.append(item)
                 i = i + 1
@@ -309,12 +254,13 @@ def main():
             url = quote['url']
             try:
                 logger.debug(url)
-                res = requests.get(url, timeout=request_timeout, headers=request_headers)
-                res.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                logger.warning(e)
+                resp = requests.get(
+                    url, timeout=request_timeout, headers=request_headers)
+                resp.raise_for_status()
+            except requests.exceptions.RequestException as e_req:
+                logger.warning(e_req)
                 continue
-            entry = res.json()
+            entry = resp.json()
             if not quote['content'] in entry or not entry[quote['content']]:
                 continue
             if not quote['author'] in entry or not entry[quote['author']]:
@@ -333,7 +279,68 @@ def main():
             items.append(item)
             logger.debug(item)
 
-    publish_md(items)
+    categories_obj = {}
+    tags_obj = {}
+    today_str = datetime.datetime.today().strftime('%Y%m%d')
+    fname_daily_box = f'archives/daily-box-{today_str}.md'
+    md_daily_box = f'# Daily Box {today_str}\n\n'
+
+    for item in items:
+        category = item['category']
+        if not category in categories_obj:
+            categories_obj[category] = ''
+
+        tags = item['tags'] if 'tags' in item else []
+        for tag in tags:
+            if not tag in tags_obj:
+                tags_obj[tag] = ''
+
+        md_entry = ''
+        if '语录' == category:
+            if item['origin']:
+                md_entry = f'- "{item["content"]}" - {item["author"]} 《{item["origin"]}》\n'
+            else:
+                md_entry = f'- "{item["content"]}" - {item["author"]}\n'
+        else:
+            md_entry = f'- [{item["channel"]}]({item["portal"]}) | [{item["title"]}]({item["link"]})\n'
+
+        categories_obj[category] += md_entry
+        for tag in tags:
+            tags_obj[tag] += md_entry
+
+    for category, category_val in categories_obj.items():
+        md_daily_box += f'## {category}\n'
+        md_daily_box += f'{category_val}\n'
+        md_category = f'## {today_str}\n{category_val}\n'
+        Path('categories').mkdir(parents=True, exist_ok=True)
+        fname_category = f'categories/{category}.md'
+        md_old = ''
+        if os.path.isfile(fname_category):
+            with open(fname_category, mode='r', encoding='utf-8') as fd_category_old:
+                md_old = fd_category_old.read()
+        with open(fname_category, mode='w', encoding='utf-8') as fd_category_new:
+            fd_category_new.write(f'{md_category}{md_old}')
+
+    md_daily_box += 'EOF'
+    print(md_daily_box)
+
+    Path('archives').mkdir(parents=True, exist_ok=True)
+    fd_daily_box = open(fname_daily_box, mode='w', encoding='utf-8')
+    fd_daily_box.write(md_daily_box)
+    fd_daily_box.close()
+    shutil.copy(fname_daily_box, 'README.md')
+
+    for tag, tag_val in tags_obj.items():
+        md_tag = f'## {today_str}\n{tag_val}\n'
+        Path('tags').mkdir(parents=True, exist_ok=True)
+        fname_tag = f'tags/{tag}.md'
+        md_old = ''
+        if os.path.isfile(fname_tag):
+            with open(fname_tag, mode='r', encoding='utf-8') as fd_tag_old:
+                md_old = fd_tag_old.read()
+        with open(fname_tag, mode='w', encoding='utf-8') as fd_tag_new:
+            fd_tag_new.write(f'{md_tag}{md_old}')
+
 
 
 if __name__ == '__main__':
